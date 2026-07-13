d08eaf89cdd7779308d8aa0c65e322cc419889c5
Phan Van Manh <manhphanvp112@gmail.com>
2026-07-14 00:08:37 +0700

    Ngày 5 : Học về kỹ thuật population trong mongoose

--- backend-day5/liThuyet.js
+++ backend-day5/liThuyet.js
@@ -8,17 +8,72 @@
     // 5. Plugins - Tái sử dụng logic Schema
     // 6. Middleware và virtuals - Can thiệp vòng đời của document và trường ảo 
 
+// 0. Ôn lại kiến thức cơ bản về mongoose 
+
+    // 0. mongoose : là thư viện cung cấp bởi node.js giúp tương tác với mongoDB
+    const mongoose = require('mongoose');
+
+    // 1. Kết nối tới 1 database cụ thể 
+    mongoose.connect('mongodb://localhost:27017/shop'); 
+    // -> Kết nối tới 1 server mongodb quản lí nhiều database và kết nối với database shop
+
+    // 2. Định nghĩa schema : là cấu trúc của 1 Document (phần khung chứa các định nghĩa , ràng buộc về dữ liệu có thể được lưu trữ)
+    const userSchema = new mongoose.Schema({name : string , age : number});
+
+    // 3. Tạo model : được định nghĩa dựa trên 1 schema , ánh xạ tới 1 collection trong database 
+    const User = mongoose.model('User' , userSchema);
+    // -> model liên kết với 1 collection , khi model thực hiện thao tác (thêm , sửa , xóa.. 1 dữ liệu) thì Collection bên trong database sẽ thực hiện thật 
+
+    // 4. Tạo và lưu Document vào Collection
+    const user1 = new User({name : 'alice' , age : 18});
+    await user1.save();
+    // -> user1 là 1 Document được tạo lên từ model User dựa trên cấu trúc schema , sau đó Document này được thêm vào Collection users bên trong database
+
 // 1 . Population (populate()) - Thay thế tham chiếu bằng dữ liệu thật 
 
     // - Bản chất : 
-        // + Khi bạn thiết kế các collection có mối quan hệ , thay vì nhúng toàn bộ document này vào document kìa (gây dư thừa) , bạn
-        // chỉ cần lưu trường ObjectId để tham chiếu đến document trong collection khác 
+        // + Khi bạn thiết kế các collection có mối quan hệ , thay vì nhúng toàn bộ document này vào document kia (gây dư thừa) , 
+        // bạn chỉ cần lưu trường ObjectId để tham chiếu document trong collection khác
+        // -> khi mà 1 document ở 1 collection khác muốn tham chiếu tới 1 document ở 1 collection khác thay vì nhúng trực tiếp vào
+        // thì sủ dụng kĩ thuật population 
 
-        // + populate() là phương thức của Mongoose giúp tự động thay thế các ObjectId bằng toàn bộ document thực tế từ collection 
-        // được tham chiếu -> tương tự JOIN trong SQL 
+        // + Hàm populate() là phương thức của mongoose giúp tự động thay thế ObjectId bằng toàn bộ document thực tế từ collection được
+        // tham chiếu 
 
     // - Cách hoạt động : 
-        // + Trong Schema , bạn khai báo một trường kiểu mongoose.Schema.Types.ObjectId kèm thuộc tính ref chỉ đến tên Model tham chiếu 
+        // + Lúc định nghĩa Schema bạn khai báo 1 trường kiểu mongoose.Schema.Types.ObjectId kèm thuộc tính ref chỉ đến tên Model tham chiếu
+        // -> lúc định nghĩa 1 Schema cần thêm các trường này để lấy dữ liệu chính xác 
         
-        // + Khi truy vấn , bạn gọi .populate('fieldName') . Mongoose sẽ thực hiện thêm truy vấn đến  collection kia lấy document có
-        // _id khớp rồi gán vào field đó 
+        // + Khi truy vần , bạn gọi .populate('fieldName') , Mongoose sẽ thực hiện truy vấn đến collection kia và lấy document có id khớp sau đó gán vào field đó
+        // -> khi lấy dữ liệu của document chứa dữ liệu này thì cần gọi thêm phương thức populate()
+
+    // vd : 
+
+        // 1. Định nghĩa Schema của category
+        const categorySchema = new mongoose.Schema({name : String});
+        // -> Tạo model dựa trên schema này , liên kết collection trong database
+        const Category = mongoose.model('Category' , categorySchema);
+
+        // 2. Định nghĩa Schema của product
+        const productSchema = new mongoose.Schema({
+            name : String,
+            price : Number,
+            // Định nghĩa trường nhận dữ liệu từ 1 document của collection khác
+            category : {
+                type : mongoose.Schema.Types.ObjectId,
+                ref : 'Category'
+            }
+        })
+        // -> Tạo model dựa trên schema này 
+        const Product = mongoose.model('Product' , productSchema);
+
+        // 3. Truy vấn dữ liệu và populate thông tin category bên trong 1 product
+        const products = await Product.find().populate('category');
+        // -> kết quả in ra sẽ là product có thêm trường category chứa thông tin 1 document category đầy đủ 
+
+    // - Khi nào sử dụng : 
+        // + Khi chỉ lấy dữ liệu từ 1 document mà bên trong nó chứa các trường từ document ở các collection khác có liên quan 
+        // + Muốn API đơn giản không cần viết thêm logic thủ công để ghép dữ liệu
+        // + Phù hợp với các truy vấn thông thường , không yêu cầu xử lí thống kê phức tạp 
+
+// 2. Aggregation Pipeline - Xử lý dữ liệu nâng cao 
